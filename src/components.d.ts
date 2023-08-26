a8ecff674eaa8c5edadf7ea63491dd61a48b0e5c
ajormoses <ajormoses@gmail.com>
2023-08-26 16:20:38 +0100

    update: progressBar vue file created

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,6 +7,7 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    ProgressBar: typeof import('./components/ProgressBar.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,11 @@
   <div
     class="w-full h-screen flex items-center place-content-center m-auto bg-white"
   >
-    <div class="">My Name is Moses</div>
+    <div class="container">
+      <div class="flex items-center place-content-center m-auto">
+        <img src="/img/logo.svg" alt="logo" />
+      </div>
+    </div>
   </div>
 </template>
 <style scoped></style>
