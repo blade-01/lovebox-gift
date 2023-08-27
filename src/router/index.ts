f9003cef83c38fd5b75506482a37d4818cbf9894
ajormoses <ajormoses@gmail.com>
2023-08-27 17:52:05 +0100

    update: homwGift vue file created

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import HomeUnboxed from "../views/HomeUnboxed.vue";
+import HomeGift from "../views/HomeGift.vue";
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: "/home-unboxed",
     name: "home-unboxed",
     component: () => HomeUnboxed,
+  },
+  {
+    path: "/home-gift",
+    name: "home-gift",
+    component: () => HomeGift,
   },
 ];
 
