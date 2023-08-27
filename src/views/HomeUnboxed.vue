c11d68cc1365b5cd39e41aa9c0825ec20cd0f7dc
ajormoses <ajormoses@gmail.com>
2023-08-27 16:57:45 +0100

    update: added home route to logo

--- src/views/HomeUnboxed.vue
+++ src/views/HomeUnboxed.vue
@@ -3,10 +3,21 @@
     class="w-full h-screen relative flex flex-col items-center place-content-center m-auto bg-white overflow-hidden"
   >
     <div class="container">
-      <img class="mx-auto mb-12" src="/img/logo.svg" alt="logo" />
+      <router-link :to="{ name: 'home' }">
+        <img
+          class="mx-auto mb-3 relative z-[9999]"
+          src="/img/logo.svg"
+          alt="logo"
+      /></router-link>
       <div
-        class="flex flex-col items-center place-content-center m-auto bg-white shadow-xl rounded-3xl py-10 px-6 md:w-[488px] relative z-[9999]"
+        class="flex flex-col items-center place-content-center m-auto bg-white shadow-xl rounded-3xl p-6 md:w-[488px] relative z-[9999]"
       >
+        <div class="progress-bar">
+          <div class="progress" :style="{ width: `${progress}%` }"></div>
+        </div>
+        <h1 class="font-semibold text-center text-lg md:text-2xl text-black">
+          Lovebox unboxed!
+        </h1>
         <img
           class="w-auto md:w-[320px] md:h-[320px]"
           src="/img/love-unboxed.svg"
@@ -54,14 +65,27 @@ onMounted(() => {
     return () => clearInterval(intervalidCircle);
   });
 });
+
+// progress bar
+const progress = ref(30);
+
+const incrementProgress = () => {
+  progress.value = Math.min(progress.value + 30, 100);
+};
 </script>
 
 <style scoped>
 .animation-container {
   @apply absolute w-[600px] md:w-[900px] h-[600px] md:h-[800px] flex items-center place-content-center;
 }
-
 .circle {
   @apply w-full h-full rounded-[50%] bg-cover transition-[backgroud-image_1s];
+}
+
+.progress-bar {
+  @apply h-1 bg-primary border-[1px] border-[#644AE2] rounded-lg overflow-hidden w-[250px] md:w-[280px] m-[20px];
+}
+.progress {
+  @apply h-full bg-[#644AE2];
 }
 </style>
