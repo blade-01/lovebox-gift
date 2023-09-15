86db566bfa24b38331e6ed41e1f419aae1e2b7c6
ajormoses <ajormoses@gmail.com>
2023-09-15 04:27:08 +0100

    update: closeCircle vue file created

---
+++ src/components/closeCircle.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="circle-container">
+    <div class="circle" :class="{ animate: animateCircle }"></div>
+  </div>
+</template>
+
+<script>
+const animateCircle = ref(false);
+
+onMounted(() => {
+  // Start the animation when the component is mounted
+  animateCircle.value = true;
+});
+</script>
+
+<style>
+.circle-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.circle {
+  width: 200px;
+  height: 200px;
+  background-color: blue;
+  border-radius: 50%;
+  transform: scale(1);
+  transition: transform 0.5s ease-in-out;
+}
+
+.animate {
+  transform: scale(0);
+}
+</style>
