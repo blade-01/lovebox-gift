bb5f0340d8000508dbf38df63aef24a53c8b9c66
Ajor Moses <ajormoses@gmail.com>
2023-11-04 18:29:17 +0100

    update: secBg added to tailwindCSS stylesheet

--- src/views/HomeUnboxed.vue
+++ src/views/HomeUnboxed.vue
@@ -1,42 +1,54 @@
 <template>
   <div
-    class="w-full h-full xl:h-screen mt-8 xl:mt-auto relative flex flex-col items-center place-content-center m-auto bg-white overflow-hidden"
+    class="w-full h-full xl:h-screen mt-10 xl:mt-auto relative flex flex-col items-center place-content-center m-auto bg-white overflow-hidden"
   >
     <div class="container">
       <router-link :to="{ name: 'home' }">
         <img
-          class="mx-auto mb-3 w-auto relative z-[9999]"
+          class="mx-auto mb-4 w-[144px] h-[36px] relative z-[9999]"
           src="/img/logo.svg"
           alt="logo"
       /></router-link>
-      <div
-        class="flex flex-col items-center place-content-center m-auto bg-white shadow-xl rounded-3xl py-10 px-6 xl:py-6 xl:px-2 md:w-[488px] relative z-[9999]"
-      >
-        <div class="progress-bar">
-          <div class="progress" :style="{ width: `${progress}%` }"></div>
+      <div class="p-0.5">
+        <div
+          class="flex flex-col items-center place-content-center m-auto bg-secBg shadow-xl rounded-3xl py-8 px-5 md:w-[488px] relative z-[9999] drop-shadow"
+        >
+          <div class="progress-bar">
+            <div class="progress" :style="{ width: `${progress}%` }"></div>
+          </div>
+          <h1 class="font-semibold text-center text-2xl text-priBlack">
+            Lovebox unboxed!
+          </h1>
+          <img
+            class="w-auto md:w-[391.13px] md:h-[359.69px]"
+            src="/img/love-unboxed.svg"
+            alt="love-unboxed"
+          />
+          <div class="text-center flex flex-col gap-4">
+            <p class="text-priBlack font-semibold text-base">
+              Want to guess the sender of this Lovebox?
+            </p>
+            <router-link :to="{ name: 'home-gift' }">
+              <button class="btn bg-main w-full rounded-3xl capitalize">
+                Yes
+              </button>
+            </router-link>
+            <router-link :to="{ name: 'home-lovebox-details' }">
+              <button
+                class="btn w-full bg-white border-[1px] border-main text-main rounded-3xl capitalize"
+              >
+                No, Proceed
+              </button>
+            </router-link>
+          </div>
         </div>
-        <h1 class="font-semibold text-center text-lg md:text-2xl text-black">
-          Lovebox unboxed!
-        </h1>
-        <img
-          class="w-auto md:w-[250px] md:h-[250px]"
-          src="/img/love-unboxed.svg"
-          alt="love-unboxed"
-        />
-        <div class="text-center flex flex-col gap-5">
-          <p>Want to guess the sender of this Lovebox?</p>
-          <router-link :to="{ name: 'home-gift' }">
-            <button class="btn bg-main w-full rounded-3xl capitalize">
-              Yes
-            </button>
-          </router-link>
-          <router-link :to="{ name: 'home-lovebox-details' }">
-            <button
-              class="btn w-full bg-white border-[1px] border-main text-main rounded-3xl capitalize"
-            >
-              No, Proceed
-            </button>
-          </router-link>
+        <div class="text-center pt-4">
+          <a
+            href="www.lovebox.com"
+            class="text-main font-bold text-sm underline"
+          >
+            www.lovebox.com
+          </a>
         </div>
       </div>
     </div>
@@ -103,5 +115,10 @@ const incrementProgress = () => {
 }
 .progress {
   @apply h-full bg-[#644AE2];
+}
+
+.drop-shadow {
+  box-shadow: 0px 0.7499999403953552px 2.249999761581421px 0.7499999403953552px
+    #00000026;
 }
 </style>
