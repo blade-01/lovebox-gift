6d3932f12c58ad6b208fe2b060de1989eed25f7c
Ajor <ajormoses@gmail.com>
2023-11-14 09:17:05 +0100

    update: hometrackshipment UI updated

--- src/views/HomeLoveBoxDetails.vue
+++ src/views/HomeLoveBoxDetails.vue
@@ -1,90 +1,92 @@
 <template>
-  <div v-if="isLoading === true">
-    <loader />
-  </div>
-  <div
-    v-else
-    class="w-full h-full xl:h-screen mt-8 xl:mt-auto relative flex flex-col items-center place-content-center m-auto bg-white overflow-hidden"
-  >
-    <div class="container">
-      <router-link :to="{ name: 'home' }">
-        <img
-          class="mx-auto mb-3 w-auto relative z-[9999]"
-          src="/img/logo.svg"
-          alt="logo"
-      /></router-link>
-      <div class="p-0.5 relative z-[9999]">
-        <div
-          class="flex flex-col items-center place-content-center m-auto bg-secBg shadow-xl rounded-3xl py-8 px-5 md:w-[488px] drop-shadow"
-        >
-          <div class="progress-bar">
-            <div class="progress" :style="{ width: `${progress}%` }"></div>
-          </div>
-          <h1
-            class="font-semibold text-center text-lg md:text-4xl text-black tracking-wide"
+  <div>
+    <div v-if="isLoading === true">
+      <loader />
+    </div>
+    <div
+      v-else
+      class="w-full h-full xl:h-screen mt-8 xl:mt-auto relative flex flex-col items-center place-content-center m-auto bg-white overflow-hidden"
+    >
+      <div class="container">
+        <router-link :to="{ name: 'home' }">
+          <img
+            class="mx-auto mb-3 w-auto relative z-[9999]"
+            src="/img/logo.svg"
+            alt="logo"
+        /></router-link>
+        <div class="p-0.5 relative z-[9999]">
+          <div
+            class="flex flex-col items-center place-content-center m-auto bg-secBg shadow-xl rounded-3xl py-8 px-5 md:w-[488px] drop-shadow"
           >
-            Lovebox Details
-          </h1>
-          <div class="text-center flex flex-col gap-2 my-4">
-            <img
-              class="mx-auto"
-              src="/img/lovebox-sender.svg"
-              alt="lovebox-sender"
-            />
-            <div
-              v-if="isAnonymous === false"
-              class="text-center flex flex-col gap-2"
+            <div class="progress-bar">
+              <div class="progress" :style="{ width: `${progress}%` }"></div>
+            </div>
+            <h1
+              class="font-semibold text-center text-lg md:text-4xl text-black tracking-wide"
             >
-              <h1 class="text-2xl font-semibold leading-8 text-priBlack">
-                Correct! Thoughtfully sent by
-              </h1>
-              <p class="text-priGray text-lg leading-6">“{{ senderName }}”</p>
+              Lovebox Details
+            </h1>
+            <div class="text-center flex flex-col gap-2 my-4">
+              <img
+                class="mx-auto"
+                src="/img/lovebox-sender.svg"
+                alt="lovebox-sender"
+              />
+              <div
+                v-if="isAnonymous === false"
+                class="text-center flex flex-col gap-2"
+              >
+                <h1 class="text-2xl font-semibold leading-8 text-priBlack">
+                  Correct! Thoughtfully sent by
+                </h1>
+                <p class="text-priGray text-lg leading-6">“{{ senderName }}”</p>
+              </div>
+            </div>
+            <div class="text-center flex flex-col gap-2 md:w-[300px]">
+              <p class="font-medium leading-6 text-priBlack text-[15px]">
+                Your Package Number
+              </p>
+              <button
+                class="btn bg-white border-[1px] border-main text-main md:text-lg leading-7 font-semibold w-full rounded-3xl capitalize"
+              >
+                5871630601
+              </button>
+              <p class="leading-6 font-medium py-1 text-sm">
+                To track your shipment, click
+                <span
+                  class="text-main underline font-bold cursor-pointer"
+                  @click="trackShipment"
+                  >here.</span
+                >
+              </p>
+              <p class="leading-6 font-medium py-3 text-base">
+                Tell the world about Love Box! click
+                <span
+                  class="text-main underline font-bold cursor-pointer"
+                  @click="handleRating"
+                  >here.</span
+                >
+              </p>
             </div>
           </div>
-          <div class="text-center flex flex-col gap-2 md:w-[300px]">
-            <p class="font-medium leading-6 text-priBlack text-[15px]">
-              Your Package Number
-            </p>
-            <button
-              class="btn bg-white border-[1px] border-main text-main md:text-lg leading-7 font-semibold w-full rounded-3xl capitalize"
+          <div class="text-center pt-4">
+            <a
+              href="www.lovebox.com"
+              class="text-main font-bold text-sm underline cursor-pointer"
             >
-              5871630601
-            </button>
-            <p class="leading-6 font-medium py-1 text-sm">
-              To track your shipment, click
-              <span
-                class="text-main underline font-bold cursor-pointer"
-                @click="trackShipment"
-                >here.</span
-              >
-            </p>
-            <p class="leading-6 font-medium py-3 text-base">
-              Tell the world about Love Box! click
-              <span
-                class="text-main underline font-bold cursor-pointer"
-                @click="handleRating"
-                >here.</span
-              >
-            </p>
+              www.lovebox.com
+            </a>
           </div>
         </div>
-        <div class="text-center pt-4">
-          <a
-            href="www.lovebox.com"
-            class="text-main font-bold text-sm underline cursor-pointer"
-          >
-            www.lovebox.com
-          </a>
-        </div>
       </div>
-    </div>
-    <div class="animation-container">
-      <div
-        class="circle"
-        :style="{
-          backgroundImage: `url(${animateCircle[currentAnimateIndex]})`,
-        }"
-      ></div>
+      <div class="animation-container">
+        <div
+          class="circle"
+          :style="{
+            backgroundImage: `url(${animateCircle[currentAnimateIndex]})`,
+          }"
+        ></div>
+      </div>
     </div>
   </div>
 </template>
--- src/views/HomeTrackShipment.vue
+++ src/views/HomeTrackShipment.vue
@@ -1,109 +1,137 @@
 <template>
-  <div
-    class="w-full h-full 2xl:h-screen mt-8 2xl:mt-auto relative flex flex-col items-center place-content-center m-auto bg-white overflow-hidden"
-  >
-    <div class="container">
-      <router-link :to="{ name: 'home' }">
-        <img class="mx-auto mb-3 w-auto relative z-[9999]" src="/img/logo.svg" alt="logo"
-      /></router-link>
-      <div class="p-0.5 relative z-[9999]">
-        <div
-          class="flex flex-col items-center place-content-center m-auto bg-secBg shadow-xl rounded-3xl py-8 px-5 md:w-[488px] drop-shadow"
-        >
-          <div class="w-full">
-            <div class="progress-bar">
-              <div class="progress" :style="{ width: `${progress}%` }"></div>
-            </div>
-            <h1
-              class="font-semibold text-center text-lg md:text-4xl text-black tracking-wide"
-            >
-              Lovebox Details
-            </h1>
-            <div class="text-center flex flex-col gap-2 my-10">
-              <img class="mx-auto" src="/img/lovebox-sender.svg" alt="lovebox-sender" />
-              <div v-if="isAnonymous === false" class="text-center flex flex-col gap-2">
-                <h1 class="text-2xl font-semibold leading-8 text-priBlack">
-                  Correct! Thoughtfully sent by
-                </h1>
-                <p class="text-priGray text-lg leading-6 capitalize">
-                  “{{ senderName }}”
-                </p>
+  <div>
+    <div v-if="isLoading === true">
+      <loader />
+    </div>
+    <div
+      class="w-full h-full 2xl:h-screen mt-8 2xl:mt-auto relative flex flex-col items-center place-content-center m-auto bg-white overflow-hidden"
+    >
+      <div class="container">
+        <router-link :to="{ name: 'home' }">
+          <img
+            class="mx-auto mb-3 w-auto relative z-[9999]"
+            src="/img/logo.svg"
+            alt="logo"
+        /></router-link>
+        <div class="p-0.5 relative z-[9999]">
+          <div
+            class="flex flex-col items-center place-content-center m-auto bg-secBg shadow-xl rounded-3xl py-8 px-5 md:w-[488px] drop-shadow"
+          >
+            <div class="w-full">
+              <div class="progress-bar">
+                <div class="progress" :style="{ width: `${progress}%` }"></div>
               </div>
-              <p class="font-medium text-base leading-5" v-if="notes !== null">
-                You've got a note from the sender,
-                <span class="text-main underline cursor-pointer" @click="viewNote"
-                  >view</span
-                >
-              </p>
-            </div>
-            <div class="grid grid-cols-1 gap-3">
-              <div class="flex justify-between items-center">
-                <p class="text-priBlack text-sm font-bold">Sample HTML text</p>
-                <div class="flex gap-1 items-center">
-                  <img src="/img/trash.svg" alt="trash" />
-                  <p class="text-priGray text-sm font-medium">
-                    {{ productDetails?.length }} Packages
-                  </p>
-                </div>
-              </div>
-              <div
-                class="grid grid-cols-3 place-items-center border-[1px] border-priGray rounded-lg py-2 px-4 md:py-3 md:px-6"
-                v-for="(product, index) in productDetails"
-                :key="index"
+              <h1
+                class="font-semibold text-center text-lg md:text-4xl text-black tracking-wide"
               >
-                <div class="text-center mr-auto pr-2 md:pr-0">
-                  <p class="text-sm text-priBlack font-semibold capitalize">
-                    {{ product?.type }}
+                Lovebox Details
+              </h1>
+              <div class="text-center flex flex-col gap-2 my-10">
+                <img
+                  class="mx-auto"
+                  src="/img/lovebox-sender.svg"
+                  alt="lovebox-sender"
+                />
+                <div
+                  v-if="isAnonymous === false"
+                  class="text-center flex flex-col gap-2"
+                >
+                  <h1 class="text-2xl font-semibold leading-8 text-priBlack">
+                    Correct! Thoughtfully sent by
+                  </h1>
+                  <p class="text-priGray text-lg leading-6 capitalize">
+                    “{{ senderName }}”
                   </p>
-                  <p class="text-[12px] text-priGray capitalize">Dummy Text</p>
                 </div>
-                <div class="text-center border-x-[1px] border-priGray px-2 md:px-5">
-                  <p class="text-sm text-priBlack font-semibold uppercase">
-                    {{ formatNumber(product?.price) }} NGN
-                  </p>
-                  <p class="text-[12px] text-priGray capitalize">Any Store</p>
-                </div>
-                <div class="text-center ml-auto pl-2 md:pl-0">
-                  <p class="text-sm text-priBlack font-semibold">
-                    {{ product?.name }}
-                  </p>
-                  <p class="text-[12px] text-priGray capitalize">Dummy Text</p>
-                </div>
-              </div>
-              <div class="pt-6 text-center">
-                <p class="leading-6 font-medium text-base text-center">
-                  Tell the world about Love Box! click
+                <p
+                  class="font-medium text-base leading-5"
+                  v-if="notes !== null"
+                >
+                  You've got a note from the sender,
                   <span
-                    class="text-main underline font-bold cursor-pointer"
-                    @click="handleRating"
-                    >here.</span
+                    class="text-main underline cursor-pointer"
+                    @click="viewNote"
+                    >view</span
                   >
                 </p>
-                <p class="text-base" v-if="hasDelivery === true">
-                  Tracking Number:
-                  <span class="font-bold text-[#2A3538]">N/A</span>
-                </p>
+              </div>
+              <div class="grid grid-cols-1 gap-3">
+                <div class="flex justify-between items-center">
+                  <p class="text-priBlack text-sm font-bold">
+                    Sample HTML text
+                  </p>
+                  <div class="flex gap-1 items-center">
+                    <img src="/img/trash.svg" alt="trash" />
+                    <p class="text-priGray text-sm font-medium">
+                      {{ productDetails?.length }} Packages
+                    </p>
+                  </div>
+                </div>
+                <div
+                  class="grid grid-cols-3 place-items-center border-[1px] border-priGray rounded-lg py-2 px-4 md:py-3 md:px-6"
+                  v-for="(product, index) in productDetails"
+                  :key="index"
+                >
+                  <div class="text-center mr-auto pr-2 md:pr-0">
+                    <p class="text-sm text-priBlack font-semibold capitalize">
+                      {{ product?.type }}
+                    </p>
+                    <p class="text-[12px] text-priGray capitalize">
+                      Dummy Text
+                    </p>
+                  </div>
+                  <div
+                    class="text-center border-x-[1px] border-priGray px-2 md:px-5"
+                  >
+                    <p class="text-sm text-priBlack font-semibold uppercase">
+                      {{ formatNumber(product?.price) }} NGN
+                    </p>
+                    <p class="text-[12px] text-priGray capitalize">Any Store</p>
+                  </div>
+                  <div class="text-center ml-auto pl-2 md:pl-0">
+                    <p class="text-sm text-priBlack font-semibold">
+                      {{ product?.name }}
+                    </p>
+                    <p class="text-[12px] text-priGray capitalize">
+                      Dummy Text
+                    </p>
+                  </div>
+                </div>
+                <div class="pt-6 text-center">
+                  <p class="leading-6 font-medium text-base text-center">
+                    Tell the world about Love Box! click
+                    <span
+                      class="text-main underline font-bold cursor-pointer"
+                      @click="handleRating"
+                      >here.</span
+                    >
+                  </p>
+                  <p class="text-base" v-if="hasDelivery === true">
+                    Tracking Number:
+                    <span class="font-bold text-[#2A3538]">N/A</span>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="text-center pt-4">
-          <a
-            href="www.lovebox.com"
-            class="text-main font-bold text-sm underline cursor-pointer"
-          >
-            www.lovebox.com
-          </a>
+          <div class="text-center pt-4">
+            <a
+              href="www.lovebox.com"
+              class="text-main font-bold text-sm underline cursor-pointer"
+            >
+              www.lovebox.com
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="animation-container">
-      <div
-        class="circle"
-        :style="{
-          backgroundImage: `url(${animateCircle[currentAnimateIndex]})`,
-        }"
-      ></div>
+      <div class="animation-container">
+        <div
+          class="circle"
+          :style="{
+            backgroundImage: `url(${animateCircle[currentAnimateIndex]})`,
+          }"
+        ></div>
+      </div>
     </div>
   </div>
 </template>
@@ -116,7 +144,7 @@ import { useFormatter } from "../composables/useFormatter";
 // use formatter for number
 const { formatNumber } = useFormatter();
 // get order details and data from store
-const { getOrderDetails, data } = useStore();
+const { getOrderDetails, data, isLoading } = useStore();
 const isAnonymous = computed(() => {
   return data.value?.isAnonymous;
 });
@@ -164,7 +192,8 @@ const animateCircle: string[] = [
 const currentAnimateIndex = ref(0);
 
 const changeImage = () => {
-  currentAnimateIndex.value = (currentAnimateIndex.value + 1) % animateCircle.length;
+  currentAnimateIndex.value =
+    (currentAnimateIndex.value + 1) % animateCircle.length;
 };
 
 // Automatically change image every 3 second
